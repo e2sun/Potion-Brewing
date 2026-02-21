940d62188bf5db262a9836b4d153b43aba5efc5f
Evelyn Sun <evelynsun632@gmail.com>
2026-02-21 13:16:44 -0500

    potion service

---
+++ frontend/src/app/components/create-potion-modal/create-potion-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-potion-modal',
+  imports: [],
+  templateUrl: './create-potion-modal.component.html',
+  styleUrl: './create-potion-modal.component.scss'
+})
+export class CreatePotionModalComponent {
+
+}
---
+++ frontend/src/app/components/potion-detail/potion-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-potion-detail',
+  imports: [],
+  templateUrl: './potion-detail.component.html',
+  styleUrl: './potion-detail.component.scss'
+})
+export class PotionDetailComponent {
+
+}
